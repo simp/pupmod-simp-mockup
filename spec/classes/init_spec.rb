b16d8e8b2bedd3cf8c0100630a8f47fe971b1b90
Steven Pritchard <steve@sicura.us>
2024-12-17 09:51:11 -0600

    Clean up for rubocop

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'dummy' do
-  shared_examples_for "a structured module" do
+  shared_examples_for 'a structured module' do
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to create_class('dummy') }
     it { is_expected.to contain_class('dummy') }
@@ -20,53 +20,63 @@ describe 'dummy' do
           os_facts
         end
 
-        context "dummy class without any parameters" do
-          let(:params) {{ }}
-          it_behaves_like "a structured module"
+        context 'dummy class without any parameters' do
+          let(:params) { {} }
+
+          it_behaves_like 'a structured module'
           it { is_expected.to contain_class('dummy').with_trusted_nets(['127.0.0.1/32']) }
         end
 
-        context "dummy class with firewall enabled" do
-          let(:params) {{
-            :firewall => true
-          }}
+        context 'dummy class with firewall enabled' do
+          let(:params) do
+            {
+              firewall: true
+            }
+          end
 
-          ###it_behaves_like "a structured module"
+          # ##it_behaves_like "a structured module"
           it { is_expected.to contain_class('dummy::config::firewall') }
 
           it { is_expected.to contain_class('dummy::config::firewall').that_comes_before('Class[dummy::service]') }
-          it { is_expected.to create_iptables__listen__tcp_stateful('allow_dummy_tcp_connections').with_dports(9999)
+          it {
+            is_expected.to create_iptables__listen__tcp_stateful('allow_dummy_tcp_connections').with_dports(9999)
           }
         end
 
-        context "dummy class with selinux enabled" do
-          let(:params) {{
-            :selinux => true
-          }}
+        context 'dummy class with selinux enabled' do
+          let(:params) do
+            {
+              selinux: true
+            }
+          end
 
-          ###it_behaves_like "a structured module"
+          # ##it_behaves_like "a structured module"
           it { is_expected.to contain_class('dummy::config::selinux') }
           it { is_expected.to contain_class('dummy::config::selinux').that_comes_before('Class[dummy::service]') }
           it { is_expected.to create_notify('FIXME: selinux') }
         end
 
-        context "dummy class with auditing enabled" do
-          let(:params) {{
-            :auditing => true
-          }}
+        context 'dummy class with auditing enabled' do
+          let(:params) do
+            {
+              auditing: true
+            }
+          end
 
-          ###it_behaves_like "a structured module"
+          # ##it_behaves_like "a structured module"
           it { is_expected.to contain_class('dummy::config::auditing') }
           it { is_expected.to contain_class('dummy::config::auditing').that_comes_before('Class[dummy::service]') }
           it { is_expected.to create_notify('FIXME: auditing') }
         end
 
-        context "dummy class with logging enabled" do
-          let(:params) {{
-            :logging => true
-          }}
+        context 'dummy class with logging enabled' do
+          let(:params) do
+            {
+              logging: true
+            }
+          end
 
-          ###it_behaves_like "a structured module"
+          # ##it_behaves_like "a structured module"
           it { is_expected.to contain_class('dummy::config::logging') }
           it { is_expected.to contain_class('dummy::config::logging').that_comes_before('Class[dummy::service]') }
           it { is_expected.to create_notify('FIXME: logging') }
@@ -77,16 +87,18 @@ describe 'dummy' do
 
   context 'unsupported operating system' do
     describe 'dummy class without any parameters on Solaris/Nexenta' do
-      let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
-        :os => {
-          :family => 'Solaris',
-          :name   => 'Nexenta',
+      let(:facts) do
+        {
+          osfamily: 'Solaris',
+       operatingsystem: 'Nexenta',
+       os: {
+         family: 'Solaris',
+         name: 'Nexenta',
+       }
         }
-      }}
+      end
 
-      it { expect { is_expected.to contain_package('dummy') }.to raise_error(Puppet::Error, /'Nexenta' is not supported/) }
+      it { expect { is_expected.to contain_package('dummy') }.to raise_error(Puppet::Error, %r{'Nexenta' is not supported}) }
     end
   end
 end
