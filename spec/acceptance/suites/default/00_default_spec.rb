215a1210f17432024151bf74f9d554d14594be5c
Steven Pritchard <steven.pritchard@gmail.com>
2025-09-15 14:20:50 -0500

    Clean up for rubocop (#95)
    * Clean up for rubocop
    * Bump simp-rake-helpers and simp-beaker-helpers dependencies

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -3,22 +3,21 @@ require 'spec_helper_acceptance'
 test_name 'dummy class'
 
 describe 'dummy class' do
-  let(:manifest) {
+  let(:manifest) do
     <<-EOS
       class { 'dummy': }
     EOS
-  }
+  end
 
   context 'default parameters' do
     # Using puppet_apply as a helper
-    it 'should work with no errors' do
-      apply_manifest(manifest, :catch_failures => true)
+    it 'works with no errors' do
+      apply_manifest(manifest, catch_failures: true)
     end
 
-    it 'should be idempotent' do
-      apply_manifest(manifest, :catch_changes => true)
+    it 'is idempotent' do
+      apply_manifest(manifest, catch_changes: true)
     end
-
 
     describe package('dummy') do
       it { is_expected.to be_installed }
